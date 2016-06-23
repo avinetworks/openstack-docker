5740b07a0ac2cbe28299741f39e1fb5026d78dfc
Praveen Yalagandula <ypraveen@avinetworks.com>
2016-06-23 21:29:24 +0000

    Docker file and all needed files to create a liberty openstack controller container

---
+++ liberty-docker/files/admin-openrc.sh
@@ -0,0 +1,8 @@
+export OS_PROJECT_DOMAIN_ID=default
+export OS_USER_DOMAIN_ID=default
+export OS_PROJECT_NAME=admin
+export OS_TENANT_NAME=admin
+export OS_USERNAME=admin
+export OS_PASSWORD=avi123
+export OS_AUTH_URL=http://localhost:35357/v3
+export OS_IDENTITY_API_VERSION=3

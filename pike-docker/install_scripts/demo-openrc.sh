9b27d21e15e27ee6f38e2916af673ebd687ae4f6
Anant Patil <ananta.v.patil@gmail.com>
2018-05-25 15:05:33 +0530

    Pike support

---
+++ pike-docker/install_scripts/demo-openrc.sh
@@ -0,0 +1,8 @@
+export OS_PROJECT_DOMAIN_ID=default
+export OS_USER_DOMAIN_ID=default
+export OS_PROJECT_NAME=demo
+export OS_TENANT_NAME=demo
+export OS_USERNAME=demo
+export OS_PASSWORD=avi123
+export OS_AUTH_URL=http://localhost:5000/v3
+export OS_IDENTITY_API_VERSION=3

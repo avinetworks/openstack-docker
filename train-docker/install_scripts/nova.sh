cc716bc3c87ba3839dbd4161bfaf795af4572567
Rishabh-Baranwal <rishabhb@vmware.com>
2020-06-08 12:50:25 +0530

    Train Support

---
+++ train-docker/install_scripts/nova.sh
@@ -0,0 +1,58 @@
+set -x
+set -e
+
+source /root/admin-openrc.sh
+echo "127.0.0.1  openstack-controller" >> /etc/hosts
+export DEBIAN_FRONTEND=noninteractive
+service mysql restart
+#service rabbitmq-server start
+while [ -n $(service rabbitmq-server start) ];
+do
+    sleep 1
+done
+service memcached restart
+service apache2 restart
+
+mysql -u root --password="avi123" -e "CREATE DATABASE placement;"
+mysql -u root --password="avi123" -e "GRANT ALL PRIVILEGES ON placement.* TO 'placement'@'localhost' IDENTIFIED BY 'avi123';"
+mysql -u root --password="avi123" -e "GRANT ALL PRIVILEGES ON placement.* TO 'placement'@'%' IDENTIFIED BY 'avi123';"
+
+openstack user create --domain default --password avi123 placement
+openstack role add --project service --user placement admin
+openstack service create --name placement --description "Placement API" placement
+openstack endpoint create --region RegionOne placement public http://openstack-controller:8778
+openstack endpoint create --region RegionOne placement internal http://openstack-controller:8778
+openstack endpoint create --region RegionOne placement admin http://openstack-controller:8778
+
+apt-get -y install placement-api
+cp /root/install_scripts/placement.conf /etc/placement/
+su -s /bin/sh -c "placement-manage db sync" placement
+service apache2 restart
+
+#add nova for cloud connector to work
+mysql -u root --password="avi123" -e "CREATE DATABASE nova;" 
+mysql -u root --password="avi123" -e "GRANT ALL PRIVILEGES ON nova.* TO 'nova'@'localhost' IDENTIFIED BY 'avi123';"
+mysql -u root --password="avi123" -e "GRANT ALL PRIVILEGES ON nova.* TO 'nova'@'%' IDENTIFIED BY 'avi123';"
+
+mysql -u root --password="avi123" -e "CREATE DATABASE nova_api;" 
+mysql -u root --password="avi123" -e "GRANT ALL PRIVILEGES ON nova_api.* TO 'nova'@'localhost' IDENTIFIED BY 'avi123';"
+mysql -u root --password="avi123" -e "GRANT ALL PRIVILEGES ON nova_api.* TO 'nova'@'%' IDENTIFIED BY 'avi123';"
+
+
+mysql -u root --password="avi123" -e "CREATE DATABASE nova_cell0;" 
+mysql -u root --password="avi123" -e "GRANT ALL PRIVILEGES ON nova_cell0.* TO 'nova'@'localhost' IDENTIFIED BY 'avi123';"
+mysql -u root --password="avi123" -e "GRANT ALL PRIVILEGES ON nova_cell0.* TO 'nova'@'%' IDENTIFIED BY 'avi123';"
+
+openstack user create --domain default --password avi123 nova
+openstack role add --project service --user nova admin
+openstack service create --name nova --description "OpenStack Compute" compute
+openstack endpoint create --region RegionOne compute public http://openstack-controller:8774/v2.1
+openstack endpoint create --region RegionOne compute internal http://openstack-controller:8774/v2.1
+openstack endpoint create --region RegionOne compute admin http://openstack-controller:8774/v2.1
+
+apt-get -y install nova-api nova-conductor nova-novncproxy nova-scheduler python-novaclient
+cp /root/install_scripts/nova.conf /etc/nova/
+su -s /bin/sh -c "nova-manage api_db sync" nova
+su -s /bin/sh -c "nova-manage cell_v2 map_cell0" nova
+su -s /bin/sh -c "nova-manage cell_v2 create_cell --name=cell1 --verbose" nova
+su -s /bin/sh -c "nova-manage db sync" nova

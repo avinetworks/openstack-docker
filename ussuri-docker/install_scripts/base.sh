250333afb5c850c40a896a796f50a89e40d27793
Anant Patil <anant@avinetworks.com>
2020-11-04 12:02:34 +0000

    Add OpenStack Ussuuri container

---
+++ ussuri-docker/install_scripts/base.sh
@@ -0,0 +1,13 @@
+set -x
+set -e
+
+apt-get --yes update
+export DEBIAN_FRONTEND=noninteractive
+apt-get --yes install software-properties-common
+add-apt-repository -y cloud-archive:ussuri
+apt-get -y update && apt-get -y upgrade && apt-get -y dist-upgrade
+apt-get install -y python3-openstackclient  python-pip git
+apt-get install -y ssh-client
+
+# Following packages needed for debugging
+apt-get install -y net-tools vim
---
+++ ussuri-docker/install_scripts/keystone.sh
@@ -0,0 +1,36 @@
+set -x
+set -e
+
+export DEBIAN_FRONTEND=noninteractive
+service mariadb restart
+
+# install keystone
+mysql -u root --password="avi123" -e "CREATE DATABASE keystone;"
+mysql -u root --password="avi123" -e "GRANT ALL PRIVILEGES ON keystone.* TO 'keystone'@'localhost' IDENTIFIED BY 'avi123';"
+mysql -u root --password="avi123" -e "GRANT ALL PRIVILEGES ON keystone.* TO 'keystone'@'%' IDENTIFIED BY 'avi123';"
+apt-get -y install apache2
+apt-get -y install libapache2-mod-wsgi
+apt-get -y install memcached
+apt-get -y install python-memcache
+apt-get -y install keystone
+cp /root/install_scripts/keystone.conf /etc/keystone/
+su -s /bin/sh -c "keystone-manage db_sync" keystone
+keystone-manage fernet_setup --keystone-user keystone --keystone-group keystone
+keystone-manage credential_setup --keystone-user keystone --keystone-group keystone
+keystone-manage bootstrap --bootstrap-password avi123 \
+  --bootstrap-admin-url http://openstack-controller:5000/v3/ \
+  --bootstrap-internal-url http://openstack-controller:5000/v3/ \
+  --bootstrap-public-url http://openstack-controller:5000/v3/ \
+  --bootstrap-region-id RegionOne
+
+service memcached restart
+service apache2 restart
+rm -f /var/lib/keystone/keystone.db
+
+echo "127.0.0.1  openstack-controller" >> /etc/hosts
+source /root/admin-openrc.sh
+openstack project create --domain default   --description "Service Project" service
+openstack project create --domain default   --description "Demo Project" demo
+openstack user create --domain default   --password avi123 demo
+openstack role create user
+openstack role add --project demo --user demo user
